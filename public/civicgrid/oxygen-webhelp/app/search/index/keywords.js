f47145f60363eb1b88aef6d27560f95356506ba9
Dom Caracappa <domcaracappa@Doms-MacBook-Air.local>
2026-09-23 00:37:14 -0400

    Add CivicGrid documentation project

---
+++ public/civicgrid/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+var keywords=[{w:"CivicGrid",p:["p0","p1","p2","p4"]},{w:"500",p:["p0","p1","p2","p4"]},{w:"Overview",p:["p0"]},{w:"Major",p:["p1"]},{w:"Subsystems",p:["p1"]},{w:"System",p:["p2"]},{w:"Architecture",p:["p2"]},{w:"Context",p:["p3"]},{w:"Sensitive",p:["p3"]},{w:"Help",p:["p3"]},{w:"Operating",p:["p4"]},{w:"Modes",p:["p4"]},{w:"Respond",p:["p5"]},{w:"to",p:["p5"]},{w:"Loss",p:["p5"]},{w:"of",p:["p5"]},{w:"Utility",p:["p5"]},{w:"Power",p:["p5"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[0, 1, 3, 4];
+ph["p2"]=[0, 1, 5, 6];
+ph["p3"]=[7, 8, 9];
+ph["p4"]=[0, 1, 10, 11];
+ph["p5"]=[12, 13, 14, 15, 16, 17];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
